f344117179403fa55cbe0c975ade004ad0347364
Vince Picone <vp@vincepic.one>
2021-10-04 21:53:15 -0500

    feat: add qr code page

---
+++ pages/qrcodes.tsx
@@ -0,0 +1,34 @@
+import { useRef, useEffect } from "react";
+import { tools } from "../lib/tools";
+import styles from "../styles/QRCodes.module.css";
+import QRCode from "qrcode";
+
+const Code: React.FC<{ id: string }> = ({ id }) => {
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    QRCode.toCanvas(
+      canvasRef.current,
+      `https://asmbly-dash.vercel.app/tools/${id}`
+    );
+  }, [id]);
+
+  return <canvas ref={canvasRef} />;
+};
+
+const QRCodesPage = () => {
+  console.log(tools);
+  return (
+    <div className={styles.container}>
+      {tools.map(({ name, id }) => {
+        return (
+          <div key={id}>
+            <Code id={id} />
+            <p>{name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default QRCodesPage;
