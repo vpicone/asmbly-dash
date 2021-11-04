fc145c992ffb3db934ed034cb2d66e03a8cfa010
Vince Picone <vp@vincepic.one>
2021-11-04 15:48:45 -0400

    chore: add assist functionality

--- components/RaiseHand.tsx
+++ components/RaiseHand.tsx
@@ -1,18 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import useWebSocket from "react-use-websocket";
 
 export const RaiseHand: React.FC<{ tool: string }> = ({ tool }) => {
-  const { sendJsonMessage, lastJsonMessage, lastMessage, readyState } =
-    useWebSocket("ws://localhost:8090");
+  const [toastId, setToastId] = useState("");
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket("ws://maker:8090");
 
   useEffect(() => {
-    if (lastJsonMessage?.data.action === "request") {
-      toast(
-        `Help has been requested at the ${lastJsonMessage.data.payload.tool}`
-      );
+    if (lastJsonMessage?.data) {
+      switch (lastJsonMessage.data.action) {
+        case "request":
+          toast(
+            (t) => (
+              <span>
+                Help has been requested at the{" "}
+                {lastJsonMessage.data.payload.tool}
+                <button
+                  onClick={() => {
+                    offerHelp(lastJsonMessage.data.payload.requestId);
+                    toast.success(
+                      `Thank you! Rendezvous at the ${lastJsonMessage.data.payload.tool} station.`,
+                      { id: t.id }
+                    );
+                  }}
+                >
+                  Help
+                </button>
+                <button onClick={() => toast.dismiss(t.id)}>Dismiss</button>
+              </span>
+            ),
+            { duration: 5000 }
+          );
+          break;
+        case "assist":
+          toast.success("Help has arrived!", {
+            id: toastId,
+          });
+          setToastId("");
+      }
     }
   }, [lastJsonMessage]);
+
+  const offerHelp = (requestId: string) => {
+    sendJsonMessage({
+      data: {
+        payload: {
+          requestId,
+        },
+        action: "assist",
+      },
+    });
+  };
 
   const requestHelp = () => {
     sendJsonMessage({
@@ -23,12 +61,21 @@ export const RaiseHand: React.FC<{ tool: string }> = ({ tool }) => {
         action: "request",
       },
     });
-    toast("Your hand has been raised!");
+    setToastId(toast.loading("Your hand has been raised!"));
   };
 
   return (
     <div style={{ position: "absolute", top: "1rem", right: "1rem" }}>
       <button onClick={requestHelp}>Request help for {tool}</button>
+      <button onClick={() => {}}>success</button>
+      <button
+        onClick={() => {
+          toast("Request for help canceled", { id: toastId });
+          setToastId("");
+        }}
+      >
+        dismiss
+      </button>
       <Toaster />
     </div>
   );
